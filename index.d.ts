e48d9ae0b0bfb0c077eafc9049f17b02759a858e
VanyaMate <buhu653@mail.ru>
2024-07-02 05:19:41 +0300

    init

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+import { Store } from '@vanyamate/sec';
+export declare const useStore: <T>(store: Store<T>) => T;
---
+++ index.ts
@@ -0,0 +1,10 @@
+import { createSignal, onCleanup } from 'solid-js';
+import { Store } from '@vanyamate/sec';
+
+
+export const useStore = function <T> (store: Store<T>) {
+    const [ state, setState ] = createSignal(store.get());
+    const unsubscribe         = store.subscribe(() => setState(() => store.get()));
+    onCleanup(unsubscribe);
+    return state();
+};
